0ab96bb8e2202d1fd95b5e6aa0e52c8865563102
swull0501-afk <s.wull0501@gmail.com>
2026-09-23 12:24:08 +0700

    Add files via upload

---
+++ src/com/pbi/tk/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pbi.tk;
+
+public class Main {
+ public static void main(String[] args)  {
+     System.out.println("===Latihan klien Server Berjalan ===");
+     System.out.println("Nama : Sri Wulan Dhari");
+     System.out.println("NIM : 25110133");
+     System.out.println("Kelas : TK.C Pagi");
+     
+ } 
+}
